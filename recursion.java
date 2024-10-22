63c58bcc5d25fc8093bf63cbaf7ee70838791068
ubuntu <uni.katakam@gmail.com>
2024-10-22 14:42:11 +0530

    new updates

--- recursion.java
+++ recursion.java
@@ -1,12 +1,12 @@
-import java.util.*
- class recursion
- {
-    public static void main(String[] args)
-    message();
- }
 
- static void message()
- {
-    System.out.println("hello world");
+import java.util.*;
+class recursion {
+    public static void main(String[] args) {
+        message();
+    }
 
- }
+    static void message() {
+        System.out.println("hello world");
+    }
+    
+}
