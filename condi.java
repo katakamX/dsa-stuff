f0d108bb07c0adee6d54d02cd06fadffb41378b1
katakamX <uni.katakam@gmail.com>
2025-02-10 20:01:10 +0530

    .

---
+++ condi.java
@@ -0,0 +1,18 @@
+public class condi 
+{
+    public static void main(String[] args)
+    {
+        int sal = 2400;
+        if(sal > 2399)
+        {
+            System.out.println("damm you rich $$$$$");
+        }
+
+        else {
+            System.out.println("niga yu broke xd xd xd ");
+        }
+        
+        
+    }
+    
+}
