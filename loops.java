4330d7811129b184d232c709e68cd67299844a23
katakamX <uni.katakam@gmail.com>
2025-02-11 08:50:14 +0530

    .

---
+++ loops.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class loops 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        int i;
+        for(i =0; i<=n; i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+    
+}
