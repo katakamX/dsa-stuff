9aa8cf83b58dfbb999d7b817c09a036ee726e675
katakamX <uni.katakam@gmail.com>
2025-08-29 07:58:40 +0530

    .

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter three numbers: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        int greatest = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+        System.out.println("The greatest number is: " + greatest);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+
+        scanner.close();
+    }
+}
--- condi.java
+++ condi.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
 
-public class condi 
-{
-    public static void main(String[] args)
-    {
+public class condi {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter paisa");
         int sal = sc.nextInt();
-        if(sal > 2399)
-        {
+        if (sal > 2399) {
             System.out.println("damm you rich $$$$$");
         }
 
         else {
             System.out.println("niga yu broke xd xd xd ");
         }
-        
-        
+
     }
-    
+
 }
